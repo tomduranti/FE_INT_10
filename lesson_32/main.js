22a2fe558359eabcaf2ac473fa061c7b61cb4633
unknown <tommaso.duranti191@gmail.com>
2025-10-29 18:17:42 +0100

    Refine functions, add utils

--- lesson_32/main.js
+++ lesson_32/main.js
@@ -1,121 +1,71 @@
+import { openTextArea, closeTextArea, createElements, sortTable, populateTblHead, populateTblBody } from "./utils.js"
+
 // 1.Create an html-page for displaying and editing text.
 // When opening the page, the text should be displayed with div tag.
 // When clicking Ctrl+E, textarea appears instead of div and can be edited.
 // When pressing Ctrl+S, the div with edited text appears instead of textarea.
 // Don’t forget to turn off default settings for these hotkey combinations.
 
-const div = document.createElement("div");
-let text = document.createTextNode("Hello, world!");
-const textArea = document.createElement("textarea");
-const el = document.querySelector("div");
+// const div = document.createElement("div");
+// let text = document.createTextNode("");
+// const textArea = document.createElement("textarea");
+// const el = document.querySelector("div");
 
-document.body.appendChild(textArea);
-div.appendChild(text);
-document.body.appendChild(div);
+// document.body.appendChild(textArea);
+// div.appendChild(text);
+// document.body.appendChild(div);
 
-textArea.style.display = "none"
+// textArea.style.display = "none"
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    document.addEventListener("keydown", openTextArea);
-    function openTextArea(e) {
-
-        if (e.ctrlKey && e.key === "e") {
-            e.preventDefault();
-            div.style.display = "none";
-            textArea.style.display = "";
-        }
-    }
-
-    textArea.addEventListener("input", () => { text.textContent = textArea.value; })
-
-    document.addEventListener("keydown", closeTextArea);
-    function closeTextArea(e) {
-        if (e.ctrlKey && e.key === "s") {
-            e.preventDefault();
-            div.style.display = "";
-            textArea.style.display = "none";
-        }
-    }
-}
-)
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.addEventListener("keydown", (e) => openTextArea(e, div, textArea));
+//   textArea.addEventListener("input", () => { text.textContent = textArea.value; })
+//   document.addEventListener("keydown", (e) => closeTextArea(e, div, textArea));
+//   }
+// )
 
 // 2.Create an html-page with a large table.
 // When clicking the column heading, it is necessary to sort data of that column.
 // Note that numerical values should be sorted as numbers, not as strings.
-const products = [
-  { product: "MacBook Pro", price: 1200, producer: "Apple" },
-  { product: "iPhone 15", price: 800, producer: "Apple" },
-  { product: "Sony WH-1000XM5", price: 150, producer: "Sony" },
-  { product: "iPad Air", price: 600, producer: "Apple" },
-  { product: "Apple Watch Series 9", price: 250, producer: "Apple" },
-  { product: "Microsoft Surface Keyboard", price: 100, producer: "Microsoft" },
-  { product: "Microsoft Surface Mouse", price: 50, producer: "Microsoft" },
-  { product: "Dell UltraSharp Monitor", price: 300, producer: "Dell" },
-  { product: "HP LaserJet Printer", price: 200, producer: "HP" },
-  { product: "WD My Passport", price: 12, producer: "Western Digital" },
-];
-// functions
-function createElements(tagName, className = "", textNode = "") {
-  const el = document.createElement(tagName);
-  if (className) el.classList.add(className);
-  if (textNode) el.appendChild(document.createTextNode(textNode));
-  return el;
-}
+// const products = [
+//   { product: "MacBook Pro", price: 1200, producer: "Apple" },
+//   { product: "iPhone 15", price: 800, producer: "Apple" },
+//   { product: "Sony WH-1000XM5", price: 150, producer: "Sony" },
+//   { product: "iPad Air", price: 600, producer: "Apple" },
+//   { product: "Apple Watch Series 9", price: 250, producer: "Apple" },
+//   { product: "Microsoft Surface Keyboard", price: 100, producer: "Microsoft" },
+//   { product: "Microsoft Surface Mouse", price: 50, producer: "Microsoft" },
+//   { product: "Dell UltraSharp Monitor", price: 300, producer: "Dell" },
+//   { product: "HP LaserJet Printer", price: 200, producer: "HP" },
+//   { product: "WD My Passport", price: 12, producer: "Western Digital" },
+// ];
 
-function sortTable(index) { //index = 0, 1, 2
+// const tbl = createElements("table");
+// const tblHead = tbl.createTHead();
+// const tr = createElements("tr", "");
+// document.body.appendChild(tbl);
+// tbl.appendChild(tblHead);
+// tblHead.appendChild(tr);
 
-  rows.sort((rowA, rowB) => {
-    const cellA = rowA.cells[index].textContent;
-    const cellB = rowB.cells[index].textContent;
-    //number sort
-    if (Number.isInteger(+cellA) && Number.isInteger(+cellB)) { return cellA - cellB; }
-    //string sort
-    return cellA.localeCompare(cellB);
-  }
-  )
-  //append sorted rows to table
-  for (const row of rows) {
-    tblBody.appendChild(row)
-  }
-}
+// // create table headers
+// const trHead = populateTblHead(products, tr);
 
-const tbl = createElements("table");
-const tblHead = tbl.createTHead();
-const tr = createElements("tr", "");
+// // create table body
+// const originalTbl = populateTblBody(products, tbl.createTBody());
+// // const originalTblCopy = originalTbl.cloneNode(true);
 
-document.body.appendChild(tbl);
-tbl.appendChild(tblHead);
-tblHead.appendChild(tr);
+// // // sort rows
+// const sortedRows = Array.from(originalTbl.querySelectorAll("tbody > tr"));
 
-// create headers
-Object.keys(products[0]).forEach((key) => {
-  const th = createElements("th", "thead");
-  th.appendChild(document.createTextNode(key));
-  tr.appendChild(th);
-});
+// // eventhandlers: TODO add unsortTable() and store original array
+// document.addEventListener("DOMContentLoaded", () => {
+//   const trHeadSelected = Array.from(trHead.querySelectorAll("tr > th"))
+//   trHeadSelected[0].addEventListener("click", function() { sortTable(0, originalTbl, sortedRows, tbl) });
+//   trHeadSelected[1].addEventListener("click", function() { sortTable(1, originalTbl, sortedRows, tbl) });
+//   trHeadSelected[2].addEventListener("click", function() { sortTable(2, originalTbl, sortedRows, tbl) });
+// })
 
-// create rows
-const tblBody = tbl.createTBody();
-for (const item of products) {
-  const row = createElements("tr");
-  Object.values(item).forEach((value) => {
-    const cell = createElements("td");
-    cell.appendChild(document.createTextNode(value));
-    row.appendChild(cell);
-  });
-  tblBody.appendChild(row);
-}
-
-// sort rows
-const tableBody = document.querySelectorAll("tbody")[0]; //object
-const rows = Array.from(tableBody.querySelectorAll("tbody > tr"));
-
-//eventhandlers: TODO add unsortTable() and store original array
-document.addEventListener("DOMContentLoaded", () => {
-  const trHead = document.querySelectorAll(".thead");
-  trHead[0].addEventListener("click", function() { sortTable(0) });
-  // trHead[0].addEventListener("click", function() { unsortTable() });
-  trHead[1].addEventListener("click", function() { sortTable(1) });
-  trHead[2].addEventListener("click", function() { sortTable(2) });
-})
+// 3.Create an html-page with a text block in a frame.
+// Execute the possibility to change the size of the block when holding the mouse cursor in the bottom right corner and pull it further.
+const frame = createElements("div", "frame", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+document.body.appendChild(frame);

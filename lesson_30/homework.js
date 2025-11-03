dea0404323f0a386442166a785dee13cc6561bd0
unknown <tommaso.duranti191@gmail.com>
2025-11-03 15:55:01 +0100

    Fix object name, parameters)

--- lesson_30/homework.js
+++ lesson_30/homework.js
@@ -1,17 +1,17 @@
 // Create an object that describes a car (manufacturer, model, year of release, average speed) and the following methods for working with this object:
 // A method that displays the car info 
 // A method for counting a time frame necessary to cover a given distance with the average speed. Note that every 4 hours the driver has to take a 1-hour break.
-const Car = {
+const car = {
     manufacturer: "Toyota",
     model: "AYGO",
     year_of_release: "2019",
     average_speed: "80",
     printProp() {
-        console.log(`Manufacturer: ${this.manufacturer},\n Model: ${this.model},\n Year of release: ${this.year_of_release},\n Average speed:${this.average_speed}`);
+        console.log(`Manufacturer: ${this.manufacturer},\n Model: ${this.model},\n Year of release: ${this.year_of_release},\n Average speed: ${this.average_speed}`);
     },
     countTime(distance) {
         // number checking
-        while (!(Number.isInteger(distance)) || distance === "" || distance <= 0) { distance = +prompt("Please input a correct integer"); }
+        while (!Boolean(Number(distance)) || distance < 0) { distance = prompt("Please input a correct integer"); }
         let drivingTime = distance / this.average_speed;
         console.log(`You can cover ${distance}km in ${Math.floor(drivingTime)} hour/s, with ${Math.floor(drivingTime / 4)} hour/s of break`);
     }

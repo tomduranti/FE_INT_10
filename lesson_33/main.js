e1fcabecfc0cd939f900019410abc2eee86f60e2
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2025-11-05 23:37:47 +0100

    Initialize files

---
+++ lesson_33/main.js
@@ -0,0 +1,18 @@
+// 1) Implement a class that describes a circle. It must have the following components:
+
+// a field that stores the radius of the circle;
+// get-property that returns the radius of the circle;
+// set-property that sets the radius of the circle;
+// get-property that returns the diameter of the circle;
+// a method that calculates the area of a circle;
+// a method that calculates the length of the circle
+
+// Demonstrate the work of properties and methods.
+
+// class Circle {
+//     constructor() {
+//         this.name = name;
+//     }
+
+//     // methods
+// }

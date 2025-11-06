bdb458b3915a7d17d8394c95c7376ac7702738a7
unknown <tommaso.duranti191@gmail.com>
2025-11-06 18:40:50 +0100

    start ex 3

--- lesson_33/main.js
+++ lesson_33/main.js
@@ -9,10 +9,48 @@
 
 // Demonstrate the work of properties and methods.
 
-// class Circle {
-//     constructor() {
-//         this.name = name;
-//     }
+class Circle {
 
-//     // methods
-// }
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get getRadius() { return this.radius; }
+    get getDiameter() { return this.radius * 2; }
+    set setRadius(value) { this.radius = value; }
+    getArea() { return Math.floor(Math.PI * (this.radius ^ 2)); }
+    getCircum() { return Math.floor(Math.PI * (this.radius * 2)); }
+
+}
+
+// 2) Implement a class that describes a stationery marker. It must have the following components:
+
+// a field that stores the color of the marker;
+// a field that stores the amount of ink in the marker (in percent);
+// an input method (accepts a string and prints the text in the corresponding color;
+// the text is displayed as long as there is ink in the marker;
+// one non-whitespace character is 0.5% of the ink in the marker).
+
+// Implement a class that describes a refillable marker. Inherit this class from a simple marker and add a method for refilling.
+class StationeryMarker {
+    constructor(color) {
+        this.color = color;
+    }
+
+    inputColor() {
+        let ink = 100;
+        const userPrompt = prompt("Write something in your favorite color");
+
+        while (ink > 0) {
+
+            for (const char of userPrompt) {
+                const p = document.getElementById("colored-text").textContent += char;
+                if (!/\s/.test(char)) ink -= 0.5;
+            }
+            console.log(ink)
+        }
+    }
+}
+
+const yellow = new StationeryMarker("yellow");
+yellow.inputColor();

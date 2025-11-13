1a6f48494f58f0d7626ca7e2773a8ddd56fbe151
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2025-11-14 00:56:04 +0100

    add getUrlData() and getUserData(), add markup

--- lesson_35/script.js
+++ lesson_35/script.js
@@ -1,6 +1,36 @@
 const API_KEY = "e07eb34967a64aa6b4c164614251311";
-const URL = "http://api.weatherapi.com/v1/current.json";
+let q = "";
+let url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=`;
 
-function getData(url, api_key) {
-    let promise = fetch(url)
+function getUserData(str, url) {
+    const strInput = document.querySelector("input[type='text']").value.toLowerCase();
+    str += strInput;
+    getUrlData(url, str);
+    document.querySelector("input[type='text']").value = ""; //clearing the input text
 }
+
+async function getUrlData(url, q) {
+    await fetch(url + q)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        })
+        .then((response) => {
+            return showData(response)}
+        )
+}
+
+//TODO: add all properties
+function showData(data) {
+    let markup = "";
+    markup += `
+        <div class="city_name">${data.location.name}, ${data.location.country}</div>
+    `;
+
+    document.body.append(markup);
+}
+

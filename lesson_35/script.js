bbc427a02e0746a6cb2b8e66419d667b4cffdbbe
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2025-11-18 00:00:34 +0100

    add all day states

--- lesson_35/script.js
+++ lesson_35/script.js
@@ -1,22 +1,14 @@
-const baseUrl = new URL("v1/current.json", "https://api.weatherapi.com/");
+import weatherDayStates from './weatherDayStates.js';
 
-//TODO: add is_day:0 to display night colors
-const state = {
-    sunny: {from: "#00C2FF", trans: "#4FC3F7", to: "#FFF7E0", color: "#0D47A1", icon: "./assets/weather_icons/113.svg"},
-    partly_cloudy: {from: "#78909C", trans:  "#B0BEC5", to: "#FFFFFF", color: "#1A237E", icon: "./assets/weather_icons/116.svg"}
-}
+async function getWeather() {
 
-function getUserData() {
     const userInput = document.querySelector("input[type='text']").value.toLowerCase();
-    getUrlData(userInput);
+    if (!userInput) return false;
     document.querySelector("input[type='text']").value = "";
-}
 
-async function getUrlData(query) {
-    
-    if (!query) return false;
+    const baseUrl = new URL("v1/current.json", "https://api.weatherapi.com/");
     baseUrl.searchParams.set("key", "e07eb34967a64aa6b4c164614251311"); //API key
-    baseUrl.searchParams.set("q", query);
+    baseUrl.searchParams.set("q", userInput);
 
     await fetch(baseUrl)
         .then(response => {
@@ -25,14 +17,14 @@ async function getUrlData(query) {
             }
         })
         .then(response => {
-            return showData(prepData(response))
+            return printWeather(response);
         })
         .catch(error => {
             console.error(error);
         })
 }
 
-function prepData(obj) {
+function printWeather(obj) {
 
     const fulltime = new Date(obj.location.localtime).toTimeString().split(" ");
 
@@ -48,7 +40,7 @@ function prepData(obj) {
         </div>
         <div class="weather_info_container__hero">
             <div class="weather_info_container__hero__img_container">
-                <img id="img_weather" alt="" src=${state[obj.current.condition.text.toLowerCase().replace(" ", "_")].icon}>
+                <img id="img_weather" alt="" src=${weatherDayStates[obj.current.condition.code].icon}>
             </div>
             <div class="weather_info_container__hero__description">
                 ${obj.current.condition.text.toLowerCase()}
@@ -59,12 +51,11 @@ function prepData(obj) {
         </div>
         `;
 
-    document.getElementById("weather_info_wrapper").style.backgroundImage = `linear-gradient(to bottom, ${state[obj.current.condition.text.toLowerCase().replace(" ", "_")].from}, ${state[obj.current.condition.text.toLowerCase().replace(" ", "_")].trans}, ${state[obj.current.condition.text.toLowerCase().replace(" ", "_")].to})`;
-    document.getElementById("weather_info_wrapper").style.color = state[obj.current.condition.text.toLowerCase().replace(" ", "_")].color;
+    document.getElementById("weather_info_wrapper").style.backgroundImage = `linear-gradient(to bottom, ${weatherDayStates[obj.current.condition.code].from}, ${weatherDayStates[obj.current.condition.code].trans}, ${weatherDayStates[obj.current.condition.code].to})`;
+    document.getElementById("weather_info_wrapper").style.color = weatherDayStates[obj.current.condition.code].color;
 
-    return markup;
+    document.getElementById("weather_info_container").innerHTML = markup;
 }
 
-function showData(data) {
-    document.getElementById("weather_info_container").innerHTML = data;
-}
+//add global scope to functions used in html
+window.getWeather = getWeather;

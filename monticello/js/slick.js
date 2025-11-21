b70e969935b2db4001939022d0f34344f3767ce9
unknown <tommaso.duranti191@gmail.com>
2025-11-21 18:01:05 +0100

    Add slicker to hero section

---
+++ monticello/js/slick.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    $(document).ready(function () {
+    $('.hero_title__text').slick({
+            dots: true,
+        });
+    });
+
+    $('.hero_title__text').slick();
+})
+

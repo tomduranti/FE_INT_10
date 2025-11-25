6ed139a859fd9cba7eca3e08ef76594ce01a2089
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2025-11-25 23:33:42 +0100

    Fixed btn hero seciotn, add function to scroll to next section

---
+++ monticello/js/main.js
@@ -0,0 +1,5 @@
+function scrollToSection() {
+    const btn = document.getElementById("scroll_down");
+    const section = document.getElementById("projects_line_breaker");
+    section.scrollIntoView();
+}

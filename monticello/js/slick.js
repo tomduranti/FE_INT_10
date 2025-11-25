92f7f863698ccdd73fdc365ec6bd9f007eae0fea
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2025-11-25 23:59:58 +0100

    Add slider to news section

--- monticello/js/slick.js
+++ monticello/js/slick.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     $(document).ready(function () {
-    $('.hero__content__text').slick({
-        vertical: true,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        infinite: true
+        $('.hero__content__text').slick({
+            vertical: true,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            infinite: true
+        });
 
-    });
+        $('.news_slider_cards').slick({
+            vertical: true,
+            dots: true,
+            arrows: true,
+            autoplay: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+
     });
 })
 

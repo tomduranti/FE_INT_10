0821461a0f7214f58f983ee7cc376b17337648a0
unknown <tommaso.duranti191@gmail.com>
2025-12-05 16:21:55 +0100

    Fix responsiveness 992px

--- monticello/js/slick.js
+++ monticello/js/slick.js
@@ -3,23 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
         $('.hero__content__text').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            vertical: true,
-            dots: false,
+            vertical: false,
+            dots: true,
             arrows: false,
             autoplay: true,
             infinite: true,
-            mobileFirst: true,
-            responsive: [
-                {
-                  breakpoint: 576,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    centerMode: false,
-                    vertical: false
-                  }
-                }
-              ]
+            mobileFirst: true
         });
 
         $('.news_slider_cards').slick({
@@ -27,21 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
             slidesToScroll: 1,
             vertical: false,
             dots: true,
-            arrows: false,
+            arrows: true,
             autoplay: false,
             infinite: true,
             mobileFirst: true,
-            // responsive: [
-            //     {
-            //       breakpoint: 375,
-            //       settings: {
-            //         slidesToShow: 1,
-            //         slidesToScroll: 1,
-            //         arrows: false,
-            //         dots: false
-            //       }
-            //     }
-            //   ]
+            responsive: [
+                {
+                  breakpoint: 992,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                  }
+                }
+              ]
         });
     });
 })
